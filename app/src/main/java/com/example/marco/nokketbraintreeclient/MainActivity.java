d2b09c9a9cbe59fa8c275c0a1f6fa26271568b30
Marco Politakis <marco.politakis@gmail.com>
2017-03-02 22:45:04 +1300

    Implemented Braintree transaction

--- app/src/main/java/com/example/marco/nokketbraintreeclient/MainActivity.java
+++ app/src/main/java/com/example/marco/nokketbraintreeclient/MainActivity.java
@@ -2,21 +2,33 @@ package com.example.marco.nokketbraintreeclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.braintreepayments.api.BraintreeFragment;
+import com.braintreepayments.api.Card;
+import com.braintreepayments.api.exceptions.InvalidArgumentException;
+import com.braintreepayments.api.interfaces.BraintreeCancelListener;
+import com.braintreepayments.api.interfaces.BraintreeErrorListener;
+import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
+import com.braintreepayments.api.models.CardBuilder;
+import com.braintreepayments.api.models.PaymentMethodNonce;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PaymentMethodNonceCreatedListener,
+        BraintreeCancelListener, BraintreeErrorListener {
 
     private String mAuthorization;
+    private BraintreeFragment mBraintreeFragment;
     private Button mDonateButton;
 
     @Override
@@ -43,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
                 try {
                     mAuthorization = response.getString("client_token");
-                    mDonateButton.setEnabled(true);
+                    onAuthorizationFetched();
                 }
                 catch (JSONException e) {
                     e.printStackTrace();
@@ -59,7 +71,82 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void onAuthorizationFetched() {
+        try {
+            mBraintreeFragment = BraintreeFragment.newInstance(this, mAuthorization);
+            mDonateButton.setEnabled(true);
+        } catch (InvalidArgumentException e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
     public void onDonate(View v) {
         Toast.makeText(MainActivity.this, "Donating...", Toast.LENGTH_LONG).show();
+
+        CardBuilder cardBuilder = new CardBuilder()
+                .cardNumber("4111111111111111")
+                //.cardNumber("4837419008371760")
+                .expirationMonth("10")
+                .expirationYear("2019");
+
+        Card.tokenize(mBraintreeFragment, cardBuilder);
+    }
+
+    @Override
+    public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
+        Toast.makeText(MainActivity.this, "Payment Method Nonce Created", Toast.LENGTH_LONG).show();
+        String nonce = paymentMethodNonce.getNonce();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("nonce", nonce);
+        client.post("http://www.nokket.com/api/nonce/transaction", params, new JsonHttpResponseHandler() {
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    super.onSuccess(statusCode, headers, response);
+
+                    try {
+                        String message = response.getString("message");
+
+                        if (message != null && message.startsWith("created")) {
+
+                            Toast.makeText(MainActivity.this,
+                                    "Payment Success: " + message, Toast.LENGTH_LONG).show();
+                        } else {
+                            if (TextUtils.isEmpty(message)) {
+
+                                Toast.makeText(MainActivity.this,
+                                        "Payment Failure: Server response was empty or malformed", Toast.LENGTH_LONG).show();
+                            } else {
+
+                                Toast.makeText(MainActivity.this,
+                                        "Payment Failure: " + message, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                    super.onFailure(statusCode, headers, throwable, errorResponse);
+
+                    Toast.makeText(MainActivity.this, "Payment Failure", Toast.LENGTH_LONG).show();
+                }
+            }
+        );
+    }
+
+    @Override
+    public void onCancel(int requestCode) {
+        Toast.makeText(MainActivity.this, "Payment Cancelled", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(Exception error) {
+        Toast.makeText(MainActivity.this, "Payment Error", Toast.LENGTH_LONG).show();
     }
 }
